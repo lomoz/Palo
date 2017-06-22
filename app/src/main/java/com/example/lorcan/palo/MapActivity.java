4c7983a5f53e85108a5ff1f9e6cbf4976755d3e8
padgm <paul.degemann@hhu.de>
2017-06-22 23:19:41 +0200

    added a map from Mapbox

--- app/src/main/java/com/example/lorcan/palo/MapActivity.java
+++ app/src/main/java/com/example/lorcan/palo/MapActivity.java
@@ -1,10 +1,14 @@
 package com.example.lorcan.palo;
 
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -15,10 +19,16 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 public class MapActivity extends AppCompatActivity {
 
     private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(getApplicationContext(), "pk.eyJ1IjoicGFsb2hodSIsImEiOiJjajQ4cDdmb2MwZjgyMnFxb2hrMnludTJkIn0.Giac8tbDSxxOu_ivc2PaUg");
+        LocationOfUser currentLocation = new LocationOfUser(this);
+        Location location = currentLocation.getLocation();
+        if (currentLocation != null) {
+            //final LatLng currLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        }
 
         setContentView(R.layout.activity_map);
         mapView = (MapView) findViewById(R.id.mapView);
@@ -29,17 +39,22 @@ public class MapActivity extends AppCompatActivity {
 
                 // Customize map with markers, polylines, etc.
 
-m
+
                 // When user clicks the map, animate to new camera location
                 mapboxMap.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(@NonNull LatLng point) {
                         CameraPosition position = new CameraPosition.Builder()
-                                .target(new LatLng(51.50550, -0.07520)) // Sets the new camera position
+                                .target(new LatLng(51.20, 6.80)) // Sets the new camera position
                                 .zoom(15) // Sets the zoom
                                 .bearing(180) // Rotate the camera
                                 .tilt(30) // Set the camera tilt
                                 .build(); // Creates a CameraPosition from the builder
+                        IconFactory iconFactory = IconFactory.getInstance(MapActivity.this);
+                        Icon icon = iconFactory.fromResource(R.drawable.positionIcon);
+                        mapboxMap.addMarker(new MarkerViewOptions()
+                                .position(new LatLng(51.20, 6.80))
+                                .icon(icon));
 
                         mapboxMap.animateCamera(CameraUpdateFactory
                                 .newCameraPosition(position), 7000);
@@ -47,7 +62,8 @@ m
                     }
                 });
 
-                final GPSTracker gps = new GPSTracker(MainActivity.this);
+
+                ;
             }
         });
     }
