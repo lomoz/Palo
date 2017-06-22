d466c0d0a2e08ffd656346260cb5ba740fa7b7f1
lomoz <lorcan.mozis@hhu.de>
2017-06-22 16:19:25 +0200

    added some elements in the xml file to show how to use them with a view element in the java class.

--- app/src/main/java/com/example/lorcan/palo/UncheckedFragment.java
+++ app/src/main/java/com/example/lorcan/palo/UncheckedFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -18,12 +20,33 @@ public class UncheckedFragment extends Fragment {
         // Required empty public constructor
     }
 
+    /*
+     * Declare elements here to handle them in the onCreateView method.
+     */
+
+    EditText etEmail, etPassword;
+    Button btnOK;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_unchecked, container, false);
+
+        /*
+         * Create and return a new View element here.
+         */
+
+        View view = inflater.inflate(R.layout.fragment_unchecked, container, false);
+
+
+        /*
+         * use the created view to get the elements from the xml file.
+         */
+
+        etEmail = (EditText)view.findViewById(R.id.etEmail);
+        etPassword = (EditText)view.findViewById(R.id.etPassword);
+        btnOK = (Button)view.findViewById(R.id.btnOK);
+
+        return view;
     }
 
 }
