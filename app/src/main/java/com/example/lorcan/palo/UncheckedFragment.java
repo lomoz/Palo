ae9a702e77e776cdd1688d93b3690e15c85e625d
lomoz <lorcan.mozis@hhu.de>
2017-06-22 16:28:21 +0200

    added an example of how to use the button OnClickListener

--- app/src/main/java/com/example/lorcan/palo/UncheckedFragment.java
+++ app/src/main/java/com/example/lorcan/palo/UncheckedFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -39,12 +40,35 @@ public class UncheckedFragment extends Fragment {
 
 
         /*
-         * use the created view to get the elements from the xml file.
+         * Use the created view to get the elements from the xml file.
          */
 
         etEmail = (EditText)view.findViewById(R.id.etEmail);
         etPassword = (EditText)view.findViewById(R.id.etPassword);
         btnOK = (Button)view.findViewById(R.id.btnOK);
+
+        /*
+         * Create an onClickListener for the button.
+         *
+         * To make i.e. a correct Toast it's important to replace "this"
+         * with "UncheckedFragment.this.getActivity()"!
+         */
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (etEmail.getText().toString().equals("palo") && etPassword.getText().toString().equals("palo")) {
+                    Toast.makeText(UncheckedFragment.this.getActivity(), "Correct", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(UncheckedFragment.this.getActivity(), "Incorrect", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
 
         return view;
     }
