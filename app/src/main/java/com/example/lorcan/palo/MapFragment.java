4fbb4dc71e3a382040199844f9303d05456f6470
lomoz <lorcan.mozis@hhu.de>
2017-06-23 00:50:13 +0200

    added working map fragment to the app.

--- app/src/main/java/com/example/lorcan/palo/MapFragment.java
+++ app/src/main/java/com/example/lorcan/palo/MapFragment.java
@@ -2,17 +2,26 @@ package com.example.lorcan.palo;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
+    GoogleMap map;
 
     public MapFragment() {
         // Required empty public constructor
@@ -23,7 +32,27 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+
+        LatLng dus = new LatLng(51.2166667, 6.7666667);
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(dus).title("Düsseldorf");
+        map.addMarker(marker);
+        map.moveCamera(CameraUpdateFactory.newLatLng(dus));
+    }
 }
