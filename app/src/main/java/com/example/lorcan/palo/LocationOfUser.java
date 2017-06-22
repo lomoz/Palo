a552ab55c9cbaccdf704b9754c1f477f9bb92580
padgm <paul.degemann@hhu.de>
2017-06-22 21:36:09 +0200

    added map of mapbox and the class of the locationListener

---
+++ app/src/main/java/com/example/lorcan/palo/LocationOfUser.java
@@ -0,0 +1,8 @@
+package com.example.lorcan.palo;
+
+/**
+ * Created by paul on 22.06.17.
+ */
+
+public class LocationOfUser {
+}
---
+++ app/src/main/java/com/example/lorcan/palo/MapActivity.java
@@ -0,0 +1,96 @@
+package com.example.lorcan.palo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+public class MapActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(getApplicationContext(), "pk.eyJ1IjoicGFsb2hodSIsImEiOiJjajQ4cDdmb2MwZjgyMnFxb2hrMnludTJkIn0.Giac8tbDSxxOu_ivc2PaUg");
+
+        setContentView(R.layout.activity_map);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(final MapboxMap mapboxMap) {
+
+                // Customize map with markers, polylines, etc.
+
+m
+                // When user clicks the map, animate to new camera location
+                mapboxMap.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng point) {
+                        CameraPosition position = new CameraPosition.Builder()
+                                .target(new LatLng(51.50550, -0.07520)) // Sets the new camera position
+                                .zoom(15) // Sets the zoom
+                                .bearing(180) // Rotate the camera
+                                .tilt(30) // Set the camera tilt
+                                .build(); // Creates a CameraPosition from the builder
+
+                        mapboxMap.animateCamera(CameraUpdateFactory
+                                .newCameraPosition(position), 7000);
+                        mapboxMap.getTrackingSettings();
+                    }
+                });
+
+                final GPSTracker gps = new GPSTracker(MainActivity.this);
+            }
+        });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+}
