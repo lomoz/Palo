92e83a09c320029398677f1de549bfb72c833d41
padgm <paul.degemann@hhu.de>
2017-06-26 22:06:16 +0200

    added location manager and location listener to MapActivity for current Location

--- app/src/main/java/com/example/lorcan/palo/MapActivity.java
+++ app/src/main/java/com/example/lorcan/palo/MapActivity.java
@@ -1,9 +1,20 @@
 package com.example.lorcan.palo;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,23 +29,34 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements LocationListener {
 
     private MapView mapView;
-    private LatLng currLocation;
+    public LatLng currLocation = null;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
     Button positionButton;
-
+    MapboxMap mapBoxGlobal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(getApplicationContext(), "pk.eyJ1IjoicGFsb2hodSIsImEiOiJjajQ4cDdmb2MwZjgyMnFxb2hrMnludTJkIn0.Giac8tbDSxxOu_ivc2PaUg");
-
-        final GPSTracker currentLocation = new GPSTracker(this);
-        currLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        System.out.println(currLocation.getLatitude() +"   "+ currLocation.getLongitude());
-
+        Mapbox.getInstance(this, "pk.eyJ1IjoicGFsb2hodSIsImEiOiJjajQ4cDdmb2MwZjgyMnFxb2hrMnludTJkIn0.Giac8tbDSxxOu_ivc2PaUg");
         setContentView(R.layout.activity_map);
+        positionButton = (Button) findViewById(R.id.positionButton);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
@@ -42,37 +64,78 @@ public class MapActivity extends AppCompatActivity {
 
             @Override
             public void onMapReady(final MapboxMap mapboxMap) {
+                mapBoxGlobal = mapboxMap;
 
-                // Customize map with markers, polylines, etc.
-                positionButton = (Button) findViewById(R.id.positionButton);
-
-                // When user clicks the button, animate to new camera location
                 positionButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        buttonClicked(mapboxMap);
+                        buttonClicked();
                     }
                 });
+
             }
         });
+
+
     }
 
-    public void buttonClicked(MapboxMap mapboxMap){
+    @Override
+    public void onLocationChanged(Location location) {
+        currLocation = new LatLng(location.getLatitude(), location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Intent gpsOptionsIntent = new Intent(
+                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(gpsOptionsIntent);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude", "enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude", "status");
+    }
+
+
+
+    public void buttonClicked() {
+        //LocationManager gets the Location Service
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        //what to do if permission is denied
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        // location Manager requesting the new position
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
         CameraPosition position = new CameraPosition.Builder()
                 .target(currLocation) // Sets the new camera position
-                .zoom(15) // Sets the zoom
-                .bearing(180) // Rotate the camera
+                .zoom(16) // Sets the zoom
+                .bearing(359) // Rotate the camera
                 .tilt(30) // Set the camera tilt
                 .build(); // Creates a CameraPosition from the builder
         IconFactory iconFactory = IconFactory.getInstance(MapActivity.this);
         Icon icon = iconFactory.fromResource(R.drawable.positionIcon);
-        mapboxMap.addMarker(new MarkerViewOptions()
-                .position(currLocation)
-                .icon(icon));
 
-        mapboxMap.animateCamera(CameraUpdateFactory
+        mapBoxGlobal.addMarker(new MarkerViewOptions()
+                    .position(currLocation)
+                    .icon(icon));
+        mapBoxGlobal.animateCamera(CameraUpdateFactory
                 .newCameraPosition(position), 7000);
-        mapboxMap.getTrackingSettings();
+        mapBoxGlobal.getTrackingSettings();
     }
     @Override
     public void onStart() {
