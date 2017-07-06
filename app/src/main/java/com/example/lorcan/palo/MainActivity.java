2bfe864e16b0cdbb15a26520fa9e5a1b1a034cb1
lomoz <lorcan.mozis@hhu.de>
2017-07-06 23:59:38 +0200

    out commented the settings actions in the app header. added new actions to the navigation drawer afor the options group.

--- app/src/main/java/com/example/lorcan/palo/MainActivity.java
+++ app/src/main/java/com/example/lorcan/palo/MainActivity.java
@@ -92,10 +92,13 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
+        /* Out commented the action for the settings in the app header.
         if (id == R.id.action_settings) {
             return true;
         }
+        */
 
         return super.onOptionsItemSelected(item);
     }
