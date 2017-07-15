9d9afbb677a1c49ab30b3a2a847a9419bbdc3525
padgm <paul.degemann@hhu.de>
2017-07-15 21:55:40 +0200

    deleted LocationListener and LocationManager from MapFragment

--- app/src/main/java/com/example/lorcan/palo/MapFragment.java
+++ app/src/main/java/com/example/lorcan/palo/MapFragment.java
@@ -35,11 +35,11 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
 
     GoogleMap map;
-    public LatLng currLocation;
+    public LatLng currLocation = new LatLng(51.6, 6.2);
     private LocationManager locationManager;
     Button positionButton;
     User user;
@@ -63,19 +63,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
 
 
 
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        //checks for permission.
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
-                        , 10);
-            }
-            return view;
-        }
-
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         return view;
     }
 
@@ -89,18 +76,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
-        //checks for permission.
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
-                        , 10);
-            }
-            return;
-        }
-
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         map = googleMap;
 
@@ -118,17 +93,21 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
             String[] array = (String[]) arrayListOtherUsers.get(i);
             LatLng locationOther = new LatLng(Double.parseDouble(array[1]), Double.parseDouble(array[2]));
 
-            //---- getting distance -----
-            float[] results = new float[1];
-            Location.distanceBetween(currLocation.latitude, currLocation.longitude, locationOther.latitude, locationOther.longitude, results);
-            float distance = results[0];
-            //----
+            if(currLocation != null) {
+                //---- getting distance -----
+                float[] results = new float[1];
+                Location.distanceBetween(currLocation.latitude, currLocation.longitude, locationOther.latitude, locationOther.longitude, results);
+                float distance = results[0];
+                //---------------------------
 
             if(distance<5){ // not ready yet!!! at this point we also need to check "isOnline" and we need to get the Status
                 LatLng position = locationOther;
                 MarkerOptions marker1 = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.locations));
                 marker1.position(position);
                 map.addMarker(marker1);
+            }
+            }else{
+                System.out.println("LOCATION IS NULL!!!!!!!!!!!!!!!!!");
             }
 
         }
@@ -150,21 +129,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
 
 
     public void buttonClicked(){
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        //what to do if permission is denied
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
-                        ,10);
-            }
-            return;
-        }
-
-        // location Manager requesting the new position
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
-
 
         user.setEmail("testmail@gmail.com");
         user.setIsOnline(true);
@@ -175,36 +139,5 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
 
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        //set current Location
-        currLocation = new LatLng(location.getLatitude(), location.getLongitude());
-
-        user.setEmail("testmail@gmail.com");
-        user.setIsOnline(true);
-        user.setLocation(currLocation);
-
-        user.updateDB();
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        //starts Settings Activity
-        Intent gpsOptionsIntent = new Intent(
-                android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-        startActivity(gpsOptionsIntent);
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-        Log.d("Latitude", "enable");
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        Log.d("Latitude", "status");
-    }
 
 }
