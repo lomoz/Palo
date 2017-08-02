2769fcb75d1f04a99828385cc32cb00f6df27414
padgm <paul.degemann@hhu.de>
2017-08-02 08:46:45 +0200

    added App Icon, Startactivity and Startsymbol

--- app/src/main/java/com/example/lorcan/palo/MapFragment.java
+++ app/src/main/java/com/example/lorcan/palo/MapFragment.java
@@ -1,15 +1,10 @@
 package com.example.lorcan.palo;
 
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +17,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -31,11 +25,18 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener {
+public class MapFragment extends Fragment implements OnMapReadyCallback, LoadDataActivity.LocationCallback {
+
+
+    public static final String TAG = MapFragment.class.getSimpleName();
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private LoadDataActivity mLoadDataActivity;
 
     GoogleMap map;
     public LatLng currLocation = new LatLng(51.6, 6.2);
-    private LocationManager locationManager;
+
     Button positionButton;
     User user;
     MarkerOptions markerOptions;
@@ -53,20 +54,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
         this.markerOptions = new MarkerOptions()
                 .position(this.currLocation)
                 .title("Status?");
-        try {
-            locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
-        }
-        catch(SecurityException e) {
-            e.printStackTrace();
-        }
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         user = new User();
 
-        /*Try to get the changed status from the ProfileFragment.
-         * Still NullPointerException
-         */
+        //Try to get the changed status from the ProfileFragment.
+         //Still NullPointerException
+
 
         //status = getArguments().getString("STATUS");
 
@@ -80,24 +75,32 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
         mapFragment.getMapAsync(this);
 
 
+        mLoadDataActivity = new LoadDataActivity(getContext(), this);
+
 
     }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mLoadDataActivity.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mLoadDataActivity.disconnect();
+    }
+
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
 
-        try {
-            locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
-        }
-        catch(SecurityException e) {
-            e.printStackTrace();
-        }
-
         map = googleMap;
-        map.moveCamera(CameraUpdateFactory.newLatLng(currLocation));
-        map.setMinZoomPreference(14);
+
 
 
         Toast.makeText(getContext(), "Current Location: " + currLocation, Toast.LENGTH_SHORT).show();
@@ -105,14 +108,25 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
         markerOptions.position(this.currLocation).title("Status?");
         map.addMarker(markerOptions);
         map.moveCamera(CameraUpdateFactory.newLatLng(this.currLocation));
+        map.moveCamera(CameraUpdateFactory.zoomBy((float) 12));
 
         user.setEmail("testmail@gmail.com");
         user.setIsOnline(true);
         user.setLocation(this.currLocation);
         user.setStatus("Test Status"); // <- insert the real status
         user.updateLocation();
-/*
-        for(int i = 0; i< arrayListOtherUsers.size(); i++){
+
+
+
+        ArrayList<String[]> arrayListOtherUsers = null;
+        String[] arrayString = new String[3];
+        arrayString[0] = "PAUL";
+        arrayString[1] = "51.2";
+        arrayString[2] = "6.2";
+        if (arrayListOtherUsers != null) {
+            arrayListOtherUsers.add(arrayString);
+
+        for(int i = 0; i< arrayListOtherUsers.size(); i++) {
 
             String[] array = (String[]) arrayListOtherUsers.get(i);
             LatLng locationOther = new LatLng(Double.parseDouble(array[1]), Double.parseDouble(array[2]));
@@ -123,18 +137,19 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
             float distance = results[0];
             //---------------------------
 
-            if(distance<100){ // not ready yet!!! at this point we also need to check "isOnline" and we need to get the Status
+            if (distance < 100) { // not ready yet!!! at this point we also need to check "isOnline" and we need to get the Status
                 LatLng position = locationOther;
                 MarkerOptions marker1 = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.mipmap.location));
                 marker1.position(position);
                 map.addMarker(marker1);
             }
+        }
+        }
 
-        }*/
-
-        try {
+       // try {
             // Customise the styling of the base map using a JSON object defined
             // in a raw resource file.
+        /*
             boolean success = map.setMapStyle(
                     MapStyleOptions.loadRawResourceStyle(
                             getContext(), R.raw.style_json));
@@ -144,28 +159,36 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
             }
         } catch (Resources.NotFoundException e) {
             Log.e("MapsActivityRaw", "Can't find style.", e);
-        }
+        }*/
     }
 
 
-    public void buttonClicked(){
-
-        user.setEmail("testmail@gmail.com");
-        user.setIsOnline(true);
-        user.setLocation(this.currLocation);
-
-        user.updateLocation();
 
 
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
+
+
+
+
     @Override
-    public void onLocationChanged(Location location) {
-        Toast.makeText(getContext(), "Current Location: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+    public void handleNewLocation(Location location) {
 
+
+        Toast.makeText(getContext(), "Current Location: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Current Location"))
         this.currLocation = new LatLng(location.getLatitude(), location.getLongitude());
         markerOptions.position(this.currLocation).title("Status?");
         map.addMarker(markerOptions);
         map.moveCamera(CameraUpdateFactory.newLatLng(this.currLocation));
+        map.moveCamera(CameraUpdateFactory.zoomBy((float) 12));
 
         user.setEmail("testmail@gmail.com");
         user.setIsOnline(true);
@@ -174,19 +197,17 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
         user.updateLocation();
     }
 
-    @Override
-    public void onProviderDisabled(String provider) {
-        Toast.makeText(getContext(), "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+
+
+
+
+
+
+    public void buttonClicked(){
+        //handleNewLocation();
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-    }
 
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
 
 }
