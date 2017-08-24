4a65ee6fdd2bc2063b8f4c2be645a99e6b418406
padgm <paul.degemann@hhu.de>
2017-08-24 16:14:15 +0200

    added sleep function from TimeUnit to show the icon for 4 seconds

--- app/src/main/java/com/example/lorcan/palo/StartActivity.java
+++ app/src/main/java/com/example/lorcan/palo/StartActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by paul on 31.07.17.
  */
@@ -15,6 +17,12 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        start();
     }
 
     public void start(){
