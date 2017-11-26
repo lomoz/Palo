6ec1a4f1730273df31deb92bc57f4ed9934fef16
lomoz <lorcan.mozis@hhu.de>
2017-11-26 22:05:29 +0100

    refactored some warnings in MapFragment.

--- app/src/main/java/com/example/lorcan/palo/StartActivity.java
+++ app/src/main/java/com/example/lorcan/palo/StartActivity.java
@@ -1,9 +1,12 @@
 package com.example.lorcan.palo;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
@@ -19,6 +22,7 @@ import com.android.volley.toolbox.Volley;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
 /**
  * Created by paul on 31.07.17.
  */
@@ -31,13 +35,26 @@ public class StartActivity extends AppCompatActivity {
     private String android_id;
     public String responseStatus;
 
+    @SuppressLint("HardwareIds")
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        android_id = telephonyManager.getDeviceId();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (telephonyManager != null) {
+            android_id = telephonyManager.getDeviceId();
+        }
         checkID(android_id);
     }
 
