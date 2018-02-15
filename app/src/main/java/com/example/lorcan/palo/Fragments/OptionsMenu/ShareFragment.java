fa534798ae953ec4de7395e496fef7137f890e52
lomoz <lorcan.mozis@hhu.de>
2018-02-15 15:09:58 +0100

    added a share function in ShareFragment as OnClick event on the ImageButton.

--- app/src/main/java/com/example/lorcan/palo/Fragments/OptionsMenu/ShareFragment.java
+++ app/src/main/java/com/example/lorcan/palo/Fragments/OptionsMenu/ShareFragment.java
@@ -1,11 +1,14 @@
 package com.example.lorcan.palo.Fragments.OptionsMenu;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.lorcan.palo.R;
 
@@ -20,10 +23,28 @@ public class ShareFragment extends Fragment {
         // Required empty public constructor
     }
 
+    ImageButton btn_share;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share, container, false);
+        View view = inflater.inflate(R.layout.fragment_share, container, false);
+
+        btn_share = (ImageButton) view.findViewById(R.id.imgBtn_share);
+
+        btn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "https://uni-duesseldorf.sciebo.de/f/50457499");
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, "Share using"));
+            }
+        });
+
+        return view;
     }
 }
