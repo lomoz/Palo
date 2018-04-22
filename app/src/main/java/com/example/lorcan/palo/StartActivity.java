771cdf0d1b68271bb364e4947bdd68ddcbfaafc9
PaulDGM <paul.deg@gmx.de>
2018-04-22 20:56:37 +0200

    set new version

--- app/src/main/java/com/example/lorcan/palo/StartActivity.java
+++ app/src/main/java/com/example/lorcan/palo/StartActivity.java
@@ -40,7 +40,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        versionControl.setActVersion("1.0.1");
+        versionControl.setActVersion("1.0.2");
 
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 
